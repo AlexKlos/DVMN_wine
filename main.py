0831fe517dca9033b94b610e52f34f561e2fc30f
AlexKlos <klos@list.ru>
2025-04-06 08:59:51 +0700

    Wrap code in def main()

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ import pandas as pd
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
 
 def pluralize_years(years):
     if 11 <= years % 100 <= 14:
@@ -26,28 +19,40 @@ def pluralize_years(years):
     return 'лет'
 
 
-winery_age = datetime.now().year - 1920
-
-wine_data = pd.read_excel('wine.xlsx', 
-                          sheet_name='Лист1', 
-                          na_values='', 
-                          keep_default_na=False,
-                          dtype=str).fillna('').to_dict(orient='records')
-
-wine_dict = defaultdict(list)
-
-for wine in wine_data:
-    category = wine.get('Категория', 'Без категории')
-    wine_dict[category].append(wine)
-
-rendered_page = template.render(
-    winery_age = winery_age,
-    years = pluralize_years(winery_age),
-    wine_data = wine_dict,
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
     
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    template = env.get_template('template.html')
+  
+    winery_age = datetime.now().year - 1920
+    
+    wine_data = pd.read_excel('wine.xlsx', 
+                              sheet_name='Лист1', 
+                              na_values='', 
+                              keep_default_na=False,
+                              dtype=str).fillna('').to_dict(orient='records')
+    
+    wine_dict = defaultdict(list)
+    
+    for wine in wine_data:
+        category = wine.get('Категория', 'Без категории')
+        wine_dict[category].append(wine)
+    
+    rendered_page = template.render(
+        winery_age = winery_age,
+        years = pluralize_years(winery_age),
+        wine_data = wine_dict,
+    )
+    
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+        
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
